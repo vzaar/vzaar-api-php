5772fd47b881b1e433c44c603cf05599542479c4
Jozef Chraplewski <jozef@vzaar.com>
2014-12-01 12:09:27 +0100

    generateThumbnail added

---
+++ examples/GenerateThumbnailTest.php
@@ -0,0 +1,17 @@
+<?php
+require_once '../api_helper.php';
+
+class GenerateThumbnailTest extends PHPUnit_Framework_TestCase {
+    public static function setUpBeforeClass() {
+        Vzaar::$url = API_ENVS::get()["url"];
+        Vzaar::$token = API_ENVS::get()["user1"]["rw_token"];
+        Vzaar::$secret = API_ENVS::get()["user1"]["login"];
+    }
+    
+    public function testGenerateThumbnail() {
+        $videoId = API_ENVS::get()["user1"]["test_video_id"];
+        $res = Vzaar::generateThumbnail($videoId, 2);
+        $this->assertRegExp('/Accepted/', $res);
+    }
+}
+?>
