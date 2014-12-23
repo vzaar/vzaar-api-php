88596e561a78ded572e9056350ce4e5152048ec4
Jozef Chraplewski <jozef@vzaar.com>
2014-12-23 13:01:28 +0100

    EditVideoTest fixed

--- examples/EditVideoTest.php
+++ examples/EditVideoTest.php
@@ -8,14 +8,17 @@ class EditVideoTest extends PHPUnit_Framework_TestCase {
         Vzaar::$secret = API_ENVS::get()["user1"]["login"];
     }
     
-    public function testUploadThumbnail() {
+    public function testEditVideo() {
         $videoId = API_ENVS::get()["user1"]["test_video_id"];
         $newTitle = "api-php-" . generateRandomStr(7);
-        
+
         $vid = Vzaar::getVideoDetails($videoId, true);
         $originalTitle = $vid->title;
 
-        $this->assertNotEquals($newTitle, $originalTitle);
+        Vzaar::editVideo($videoId, $newTitle, "woof", false);
+        $vid = Vzaar::getVideoDetails($videoId, true);
+
+        $this->assertNotEquals($vid->title, $originalTitle);
     }
 }
 ?>
