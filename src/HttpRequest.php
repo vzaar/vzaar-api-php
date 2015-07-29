ede166a9923aae7a6dc9e4d506a50f0c065fbef5
Skitsanos <info@skitsanos.com>
2015-07-29 11:55:20 +0300

    update on HttpRequest

--- src/HttpRequest.php
+++ src/HttpRequest.php
@@ -9,7 +9,8 @@
 //application/json
 //text/xml
 
-class HttpRequest {
+class HttpRequest
+{
 
     protected $c;
     protected $url;
@@ -20,7 +21,8 @@ class HttpRequest {
     var $verbose = false;
     var $uploadMode = false;
 
-    function __construct($url) {
+    function __construct($url)
+    {
         if (!function_exists('curl_init')) {
             echo "Function curl_init, used by HttpRequest does not exist.\n";
         }
@@ -28,14 +30,14 @@ class HttpRequest {
         $this->c = curl_init($this->url);
     }
 
-    function send($data=null, $filepath=null) {
+    function send($data = null, $filepath = null)
+    {
         if (count($this->headers) > 0) {
             curl_setopt($this->c, CURLOPT_HEADER, false);
             curl_setopt($this->c, CURLOPT_HTTPHEADER, $this->headers);
         }
 
         curl_setopt($this->c, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($this->c, CURLOPT_FOLLOWLOCATION, true);
 
         if ($this->useSsl) {
             curl_setopt($this->c, CURLOPT_SSL_VERIFYPEER, 0);
@@ -49,8 +51,8 @@ class HttpRequest {
 
         if ($this->uploadMode) {
             //curl_setopt($this->c, CURLOPT_URL, $filepath);
-			//curl_setopt($this->c, CURLOPT_UPLOAD, true);
-			curl_setopt($this->c, CURLOPT_POST, true);
+            //curl_setopt($this->c, CURLOPT_UPLOAD, true);
+            curl_setopt($this->c, CURLOPT_POST, true);
             $fp = fopen($filepath, 'r');
             curl_setopt($this->c, CURLOPT_INFILE, $fp);
             curl_setopt($this->c, CURLOPT_INFILESIZE, filesize($filepath));
@@ -80,11 +82,44 @@ class HttpRequest {
 
         curl_setopt($this->c, CURLOPT_VERBOSE, $this->verbose);
 
-        $output = curl_exec($this->c);
+        if (ini_get('open_basedir') == '' && ini_get('safe_mode' == 'Off')) {
+            curl_setopt($this->c, CURLOPT_FOLLOWLOCATION, true);
+            $output = curl_exec($this->c);
+        } else {
+            curl_setopt($this->c, CURLOPT_FOLLOWLOCATION, false);
+            $output = $this->curlExec($this->c);
+        }
 
-        curl_close($this->c);
         return $output;
     }
 
+    function curlExec($ch)
+    {
+
+        $newUrl = '';
+        $maxRedirection = 10;
+        do {
+            if ($maxRedirection < 1) die('Error: reached the limit of redirections');
+            if (!empty($newUrl)) curl_setopt($ch, CURLOPT_URL, $newUrl); // redirect needed
+
+            $curlResult = curl_exec($ch);
+            $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+            if ($code == 301 || $code == 302 || $code == 303 || $code == 307) {
+                preg_match('/Location:(.*?)\n/', $curlResult, $matches);
+                $newUrl = trim(array_pop($matches));
+                curl_close($ch);
+
+                $maxRedirection--;
+                continue;
+            } else // no more redirection
+            {
+                $code = 0;
+                curl_close($ch);
+            }
+        } while ($code);
+        return $curlResult;
+    }
 }
+
 ?>
