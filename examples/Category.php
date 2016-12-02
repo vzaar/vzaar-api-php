0871c14a83e0365335b73c14ebe6a3508bf43fcc
Ed James <ed.james.email@gmail.com>
2016-12-02 17:37:45 +0000

    (NT) update Category.php examples with pagination

--- examples/Category.php
+++ examples/Category.php
@@ -90,4 +90,66 @@
 
     }
 
+
+    //
+    // Paginate Category
+    //
+    try {
+
+      $paramsList = array('per_page' => '2');
+
+      //this methods gets initial page
+      $pager = VzaarApi\CategoriesList::paginate($paramsList);
+
+      foreach($pager as $category) {
+        if(isset($category->id) & isset($category->name)){
+          echo PHP_EOL.'Paginate Category (first): id: ' .$category->id .' '. $category->name;
+        }
+      }
+      echo PHP_EOL;
+
+      $pager->nextPage();
+      foreach($pager as $category) {
+        if(isset($category->id) & isset($category->name)){
+          echo PHP_EOL.'Paginate Category (next): id: ' .$category->id .' '. $category->name;
+        }
+      }
+      echo PHP_EOL;
+
+      $pager->lastPage();
+      foreach($pager as $category) {
+        if(isset($category->id) & isset($category->name)){
+          echo PHP_EOL.'Paginate Category (last): id: ' .$category->id .' '. $category->name;
+        }
+      }
+      echo PHP_EOL;
+
+      $pager->previousPage();
+      foreach($pager as $category) {
+        if(isset($category->id) & isset($category->name)){
+          echo PHP_EOL.'Paginate Category (previous): id: ' .$category->id .' '. $category->name;
+        }
+      }
+      echo PHP_EOL;
+
+      $pager->firstPage();
+      foreach($pager as $category) {
+        if(isset($category->id) & isset($category->name)){
+          echo PHP_EOL.'Paginate Category (first): id: ' .$category->id .' '. $category->name;
+        }
+      }
+      echo PHP_EOL;
+
+    } catch(VzaarApi\Exceptions\VzaarException $ve) {
+
+      echo $ve->getMessage();
+
+    } catch(VzaarApi\Exceptions\VzaarError $verr) {
+
+      echo $verr->getMessage();
+
+    }
+
+    echo PHP_EOL;
+
 ?>
