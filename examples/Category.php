0907c6eae9acbc50f8dbc4ab25dcd0fdf1b26494
Ed James <ed.james.email@gmail.com>
2016-12-02 16:36:49 +0000

    (NT) update Category.php examples

--- examples/Category.php
+++ examples/Category.php
@@ -1,69 +1,93 @@
 <?php
     require 'autoload.php';
-    
+
+    //
+    // Lookup Category
+    //
     try {
-        
-        /**
-         * Category Lookup Example
-         *
-         */
-        
-        //no categories defined, Exception is thrown due to 404
-        //define category and uncomment the below line, change category ID
-        
-        //$category = VzaarApi\Category::find(1);
-        
-        /**
-         * Category Subtree Example
-         *
-         * instance level method call
-         * this returns subtree List of current category in the object
-         */
-        
-        //no categories defined, Exception is thrown due to 404
-        //after category is defined and found with above ::find()
-        //uncomment below line
-        
-        //$subInstance = $category->subtree();
-        
-        /**
-         * Category Subtree Example
-         *
-         * class level (static) method call
-         * the method returns subtree list
-         */
-        
-        //no categories defined, exception is thrown due to 404
-        //uncomment when category defined
-        
-        //$subStatic = VzaarApi\CategoriesList::subtree(1);
-        
-        /**
-         * Category List Example
-         *
-         * Iterate over recipes
-         *
-         */
-        
-        foreach(VzaarApi\CategoriesList::each_item() as $category) {
-        
-            if(isset($category->id) & isset($category->name)){
-            
-                echo PHP_EOL.'List Category: id: ' .$category->id .''. $category->name;
-            }
-        
-        }
-        
-        echo PHP_EOL;
-        
-    
-    }catch(VzaarApi\Exceptions\VzaarException $ve){
-    
-        echo $ve->getMessage();
-        
-    }catch(VzaarApi\Exceptions\VzaarError $verr){
-    
-        echo $verr->getMessage();
-        
+
+      $category = VzaarApi\Category::find($category_id);
+      echo PHP_EOL.'Lookup Category: id: ' .$category->id .' '. $category->name;
+      echo PHP_EOL;
+
+    } catch(VzaarApi\Exceptions\VzaarException $ve) {
+
+      echo $ve->getMessage();
+
+    } catch(VzaarApi\Exceptions\VzaarError $verr) {
+
+      echo $verr->getMessage();
+
     }
+
+
+    //
+    // List Category: Iterate over categories
+    //
+    try {
+
+      foreach(VzaarApi\CategoriesList::each_item() as $category) {
+        if(isset($category->id) & isset($category->name)){
+          echo PHP_EOL.'List Category (iterate): id: ' .$category->id .' '. $category->name;
+        }
+      }
+      echo PHP_EOL;
+
+    } catch(VzaarApi\Exceptions\VzaarException $ve) {
+
+      echo $ve->getMessage();
+
+    } catch(VzaarApi\Exceptions\VzaarError $verr) {
+
+      echo $verr->getMessage();
+
+    }
+
+
+    //
+    // List Category Subtree (instance)
+    //
+    try {
+
+      $top_category = VzaarApi\Category::find($category_id);
+      foreach($top_category->subtree() as $category) {
+        if(isset($category->id) & isset($category->name)){
+          echo PHP_EOL.'List Category Subtree (instance): id: ' .$category->id .' '. $category->name;
+        }
+      }
+      echo PHP_EOL;
+
+    } catch(VzaarApi\Exceptions\VzaarException $ve) {
+
+      echo $ve->getMessage();
+
+    } catch(VzaarApi\Exceptions\VzaarError $verr) {
+
+      echo $verr->getMessage();
+
+    }
+
+
+    //
+    // List Category Subtree (static)
+    //
+    try {
+
+      foreach(VzaarApi\CategoriesList::subtree($category_id) as $category) {
+        if(isset($category->id) & isset($category->name)){
+          echo PHP_EOL.'List Category Subtree (static): id: ' .$category->id .' '. $category->name;
+        }
+      }
+      echo PHP_EOL;
+
+    } catch(VzaarApi\Exceptions\VzaarException $ve) {
+
+      echo $ve->getMessage();
+
+    } catch(VzaarApi\Exceptions\VzaarError $verr) {
+
+      echo $verr->getMessage();
+
+    }
+
 ?>
--- examples/autoload.php
+++ examples/autoload.php
@@ -1,10 +1,16 @@
 <?php
     require '../lib/vzaar.php';
-    
+
     VzaarApi\Client::$client_id = $_ENV['VZAAR_CLIENT_ID'];
     VzaarApi\Client::$auth_token = $_ENV['VZAAR_AUTH_TOKEN'];
-    
+
     //outputs equest/response details
     //VzaarApi\Client::$VERBOSE = true;
+
+    // Update these values to match ones from your own account.
+    $category_id = 3206;
+    $encoding_preset_id = 3;
+    $default_ingest_recipe = 67;
+    $other_ingest_recipe = 73;
 
 ?>
